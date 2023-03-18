d6201f78514c3ffa4c6c65f467a8b9af174b44eb
messam10 <memaq1998.10.7@gmail.com>
2023-03-18 14:20:02 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,17 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let modal = document.getElementById("modal");
+modal.classList.add("hidden");
+let btn = document.querySelectorAll(".like-glyph").forEach((like) => {
+  like.addEventListener("click", (event) => {
+    let heart = event.target;
 
+    mimicServerCall()
+      .then(function () {
+        if (heart.innerText === EMPTY_HEART) {
+          heart.innerText = FULL_HEART;
+          heart.classList.add("activated-heart");
+        } else {
+          heart.innerText = EMPTY_HEART;
+          heart.classList.remove("activated-heart");
+        }
+      })
+      .catch(function (error) {
+        modal.classList.remove("hidden");
+        modal.innerText = error;
+        setTimeout(() => modal.classList.add("hidden"), 5000);
+      });
 
-
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
